b3760703cf61d35e8d78b950c2b4e4f10b371459
Carl Fremault <carlfremault@yahoo.com>
2022-03-15 09:23:55 +0100

    Phase 14 : Re-création du projet des tests unitaires afin qu'il soit positionné à l'intérieur de l'arborescence du projet principal, et pris en compte par Git

---
+++ Mediatek86UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mediatek86UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Mediatek86UnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("252e57ef-f1e0-419a-a2b8-262015c25c64")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
